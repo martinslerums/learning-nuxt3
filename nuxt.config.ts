da2df6d28f787bcb960689a8b36952c5eed2fd76
martleru <68188509+martleru@users.noreply.github.com>
2024-07-20 20:38:57 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Learning Nuxt3",
+      meta: [
+        {
+          name: "description",
+          content: "Everything about Nuxt3",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+});
